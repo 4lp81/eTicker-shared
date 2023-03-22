57680aebddf5beb9ee5414b9684108f1b91b9ab0
Alp <alp.arslan@students.fhv.at>
2023-03-22 23:50:15 +0100

    minor change

--- src/main/java/at/fhv/TeamE/shared/api/RMI_EventService.java
+++ src/main/java/at/fhv/TeamE/shared/api/RMI_EventService.java
@@ -3,8 +3,9 @@ package at.fhv.TeamE.shared.api;
 import at.fhv.TeamE.shared.dto.EventOverviewDTO;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.List;
 
 public interface RMI_EventService extends Remote {
-    List<EventOverviewDTO> searchArtistOrEvent(String searchString);
+    List<EventOverviewDTO> searchArtistOrEvent(String searchString) throws RemoteException;
 }
