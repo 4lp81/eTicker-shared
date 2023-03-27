c5ad33e5ef381be194f4b067db4e8e58178ed86e
Alp Arslan <arslanalp81@hotmail.com>
2023-03-27 13:46:59 +0200

    Added Time and availability

--- src/main/java/at/fhv/TeamE/shared/dto/EventOverviewDTO.java
+++ src/main/java/at/fhv/TeamE/shared/dto/EventOverviewDTO.java
@@ -14,18 +14,28 @@ public class EventOverviewDTO implements Serializable {
     private String description;
     private String date;
     private double price;
+    private String time;
+
+    private int availableSeat;
+
+    private int availableStanding;
+
+
 
     public EventOverviewDTO(){
 
     }
 
-    public EventOverviewDTO(UUID eventId, String title, String location, String description, String date, double price) {
+    public EventOverviewDTO(UUID eventId, String title, String location, String description, String date, double price, String time, int availableSeat, int availableStanding) {
         this.eventId = eventId;
         this.title = title;
         this.location = location;
         this.description = description;
         this.date = date;
         this.price = price;
+        this.time = time;
+        this.availableSeat = availableSeat;
+        this.availableStanding = availableStanding;
     }
 
     public UUID getEventId() {
@@ -52,16 +62,28 @@ public class EventOverviewDTO implements Serializable {
         return price;
     }
 
+    public String getTime() {
+        return time;
+    }
+
+    public int getAvailableSeat() {
+        return availableSeat;
+    }
+
+    public int getAvailableStanding() {
+        return availableStanding;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         EventOverviewDTO that = (EventOverviewDTO) o;
-        return Double.compare(that.price, price) == 0 && eventId.equals(that.eventId) && title.equals(that.title) && location.equals(that.location) && description.equals(that.description) && date.equals(that.date);
+        return Double.compare(that.price, price) == 0 && availableSeat == that.availableSeat && availableStanding == that.availableStanding && eventId.equals(that.eventId) && title.equals(that.title) && location.equals(that.location) && description.equals(that.description) && date.equals(that.date) && time.equals(that.time);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(eventId, title, location, description, date, price);
+        return Objects.hash(eventId, title, location, description, date, price, time, availableSeat, availableStanding);
     }
 }
