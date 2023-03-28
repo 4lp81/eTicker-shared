30e002631e834e2c3aed6fdbde2f298723d6b4fb
Alp <alp.arslan@students.fhv.at>
2023-03-29 00:18:26 +0200

    added sale service

--- src/main/java/at/fhv/TeamE/shared/dto/SaleDTO.java
+++ src/main/java/at/fhv/TeamE/shared/dto/SaleDTO.java
@@ -5,19 +5,26 @@ import java.util.Objects;
 import java.util.UUID;
 
 public class SaleDTO implements Serializable {
-    private final UUID saleId;
-    private final UUID productId;
-    private final String eventName;
-    private final double price;
+    private UUID saleId;
+    private UUID productId;
+    private String eventName;
+    private double price;
     private int amountPurchased;
 
+    private int amountRefunded;
 
-    public SaleDTO(UUID saleId, UUID productId, String eventName, double price, int amountPurchased) {
+    public SaleDTO(){
+
+    }
+
+
+    public SaleDTO(UUID saleId, UUID productId, String eventName, double price, int amountPurchased, int amountRefunded) {
         this.saleId = saleId;
         this.productId = productId;
         this.eventName = eventName;
         this.price = price;
         this.amountPurchased = amountPurchased;
+        this.amountRefunded = amountRefunded;
     }
 
     //Random Comment weil java fx zahlen ned mag
@@ -44,16 +51,20 @@ public class SaleDTO implements Serializable {
         return amountPurchased;
     }
 
+    public int getAmountRefunded() {
+        return amountRefunded;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         SaleDTO saleDTO = (SaleDTO) o;
-        return Double.compare(saleDTO.price, price) == 0 && amountPurchased == saleDTO.amountPurchased && saleId.equals(saleDTO.saleId) && productId.equals(saleDTO.productId) && eventName.equals(saleDTO.eventName);
+        return Double.compare(saleDTO.price, price) == 0 && amountPurchased == saleDTO.amountPurchased && amountRefunded == saleDTO.amountRefunded && saleId.equals(saleDTO.saleId) && productId.equals(saleDTO.productId) && eventName.equals(saleDTO.eventName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(saleId, productId, eventName, price, amountPurchased);
+        return Objects.hash(saleId, productId, eventName, price, amountPurchased, amountPurchased);
     }
 }
