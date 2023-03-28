b808075c6fc3b95e456bc0c8ec628ee5857bca1c
Alp <alp.arslan@students.fhv.at>
2023-03-29 01:12:22 +0200

    added sale service

--- src/main/java/at/fhv/TeamE/shared/api/RMI_SaleService.java
+++ src/main/java/at/fhv/TeamE/shared/api/RMI_SaleService.java
@@ -2,10 +2,11 @@ package at.fhv.TeamE.shared.api;
 
 import at.fhv.TeamE.shared.dto.SaleDTO;
 
+import java.rmi.Remote;
 import java.util.List;
 import java.util.UUID;
 
-public interface RMI_SaleService {
+public interface RMI_SaleService extends Remote {
     //ToDo: Refund maybe later in the second sprint...
     void purchase(UUID customerId, List<SaleDTO> salesDTO);
 }
