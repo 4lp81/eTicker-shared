c8f862c04aad0c9160375c233c32ee3076d53c09
Alp <alp.arslan@students.fhv.at>
2023-04-25 17:46:05 +0200

    added SaleHistoryDTO and rmi functions for search and refund (try7 bc number)

--- src/main/java/at/fhv/TeamE/shared/api/RMI_SaleService.java
+++ src/main/java/at/fhv/TeamE/shared/api/RMI_SaleService.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 public interface RMI_SaleService extends Remote {
-    //ToDo: Refund maybe later in the second sprint.......
+    //.
     void purchase(UUID customerId, List<SaleDTO> salesDTO) throws RemoteException;
 
     void refund(UUID saleId, List<SaleDTO> salesDTO) throws RemoteException;
