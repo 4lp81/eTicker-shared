1cbf2895391385156b8430f7f9f9463099749b0b
XIII07 <56972287+XIII07@users.noreply.github.com>
2023-07-13 10:22:34 +0200

    das fehlende getSaleType eingefügt

--- src/main/java/at/fhv/TeamE/shared/dto/SaleHistoryDTO.java
+++ src/main/java/at/fhv/TeamE/shared/dto/SaleHistoryDTO.java
@@ -121,6 +121,7 @@ public class SaleHistoryDTO implements Serializable {
 
     public SaleType getSaleType(){return saleType;}
 
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
